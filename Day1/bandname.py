220648618f96cbf92089dbb6ced0083163776d8d
souldrop <imneocoder@gmail.com>
2021-10-30 14:58:04 +0530

    Day1 completed

---
+++ Day1/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator\n")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your Band name could be " + city + " " + pet)
